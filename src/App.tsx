39da1aae376123210fac05626a7b3ea5b6477c2f
Alilya <alinakolesnikova0101@gmail.com>
2023-10-24 01:09:40 +0300

    dark and light theme toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { Route, Routes, Link } from "react-router-dom";
-import './index.scss'
+import './Components/styles/index.scss'
 import { AboutPageAsync } from "./Components/Pages/AboutPage/AboutPageAsync";
 import { MainPageAsync } from "./Components/Pages/MainPage/MainPageAsync";
-import { Suspense } from "react";
-
+import { Suspense, useContext, useState } from "react";
+import ThemeContext, { Theme } from "./Components/theme/ThemeContext";
+import { useTheme } from "./Components/theme/useTheme";
 
 
 const App = () => {
+    const {theme, toggleTheme} = useTheme();
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
             <Link to={"/"}>Главная</Link>
             <Link to={"/about"}>О сайте</Link>
+            <button onClick={toggleTheme}>сменить тему</button>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
                     <Route path={'/about'} element={<AboutPageAsync />} />
